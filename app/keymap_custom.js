d3966d9bbfa65e0a17c10217a1771a7628b96fd4
ishiduca <ishiduca@gmail.com>
2014-11-25 10:37:46 +0900

    apply combokeys

--- app/keymap_custom.js
+++ app/keymap_custom.js
@@ -1,37 +1,67 @@
 'use strict'
-
-var Keymap = require('./keymap').Keymap
-
-function KeymapCustom (target) {
-    Keymap.call(this, target)
+var Combo = require('combokeys')
+function Keymap (element) {
+    var c = new Combo(element)
+    for (var prop in c) {
+        if (c.hasOwnProperty(prop))
+            this[prop] = c[prop]
+    }
     this.helpStrs = {}
 }
+;(function () {
+    var F = function () {}
+    F.prototype = Combo.prototype
+    Keymap.prototype = new F
+    Keymap.prototype.constructor = Keymap
+})()
 
-var F = function () {}
-F.prototype = Keymap.prototype
-KeymapCustom.prototype = new F
-KeymapCustom.prototype.constructor = KeymapCustom
+Keymap.prototype.on = function (keys, help, cb, act) {
+    this.bind(keys, cb, act)
 
-KeymapCustom.prototype.on = function (chas, helpStr, listener) {
     var me = this
-    if ('string' === typeof chas) chas = [ chas ]
-    chas.forEach(function (cha) {
-        me.helpStrs[cha] = helpStr    
+    ;('[object Array]' === ({}).toString.apply(keys) ? keys : [ keys ])
+    .forEach(function (key) {
+        me.helpStrs[key] = help
     })
-
-    Keymap.prototype.on.apply(this, [chas, listener])
-
     return this
 }
-KeymapCustom.prototype.help = function (cha) {
-	return this.helpStrs[cha] || '"' + cha + '" not registered'
-}
-KeymapCustom.prototype.registers = function () {
-	return Object.keys(this.listeners)
+
+module.exports = function create (target) {
+    return new Keymap(target || document)
 }
 
-module.exports = function keymapCustom (target) {
-    return new KeymapCustom(target)
-}
-module.exports.KeymapCustom = KeymapCustom
-module.exports.Keymap       = Keymap
+//var Keymap = require('./keymap').Keymap
+//
+//function KeymapCustom (target) {
+//    Keymap.call(this, target)
+//    this.helpStrs = {}
+//}
+//
+//var F = function () {}
+//F.prototype = Keymap.prototype
+//KeymapCustom.prototype = new F
+//KeymapCustom.prototype.constructor = KeymapCustom
+//
+//KeymapCustom.prototype.on = function (chas, helpStr, listener) {
+//    var me = this
+//    if ('string' === typeof chas) chas = [ chas ]
+//    chas.forEach(function (cha) {
+//        me.helpStrs[cha] = helpStr    
+//    })
+//
+//    Keymap.prototype.on.apply(this, [chas, listener])
+//
+//    return this
+//}
+//KeymapCustom.prototype.help = function (cha) {
+//    return this.helpStrs[cha] || '"' + cha + '" not registered'
+//}
+//KeymapCustom.prototype.registers = function () {
+//    return Object.keys(this.listeners)
+//}
+//
+//module.exports = function keymapCustom (target) {
+//    return new KeymapCustom(target)
+//}
+//module.exports.KeymapCustom = KeymapCustom
+//module.exports.Keymap       = Keymap
